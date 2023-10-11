5b86d2aa219361eac613ec0274d45f535e2dd8c3
MAGANER <maganer.krik@mail.ru>
2023-10-11 07:44:41 +0300

    catch ctrl+C

--- __main__.py
+++ __main__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 import interface
+import signal
+from sys import exit
 
+def ctrlc(sig, frame):
+    exit(0)
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, ctrlc)
     interface.run()
