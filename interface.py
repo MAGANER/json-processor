2144f70c9a5956243298dfe65c1e3c5baa2af93a
MAGANER <maganer.krik@mail.ru>
2023-10-11 08:28:32 +0300

    now it's able to pass commands via cli arguments and then they will be executed one-by-one

--- interface.py
+++ interface.py
@@ -1,5 +1,7 @@
 import functions
 from functions import h
+from sys import argv
+from functools import reduce
 
 def __create_command_heads_tuple():
     heads = []
@@ -12,17 +14,32 @@ def __create_command_heads_tuple():
 def __parse_command(com):
     splitted = com.split(" ")
     return (splitted[0],splitted[1:])
-        
+
+def __run_command(command,heads):
+    command_head ,*args = __parse_command(command)
+    args = args[0] #make it flat
+    if command_head in heads:
+        functions.commands[h(command_head)](args)
+    else:
+        print("command {} doesn't exist!".format(command_head))
 def run():
     heads = __create_command_heads_tuple()
-    
-    while True:
-        command = input(">")
-        command_head ,*args = __parse_command(command)
-        args = args[0] #make it flat
+
+    if len(argv) == 1:
+        while True:
+            command = input(">")
+            __run_command(command,heads)
+    else:
+        s = reduce(lambda a,b:a+" "+b,argv[1:]) + ";"
+        commands = list(filter(lambda n: n != "", s.split(";")))
+
+        if len(commands) == 2 and commands[1] == '':
+            print("error: every command should be finished with ';'")
+            exit(-1)
+        for c in commands:
+            if c[0] == ' ':
+                c = c[1:]
+            __run_command(c,heads)
         
-        if command_head in heads:
-            functions.commands[h(command_head)](args)
-        else:
-            print("command {} doesn't exist!".format(command_head))
+            
         
